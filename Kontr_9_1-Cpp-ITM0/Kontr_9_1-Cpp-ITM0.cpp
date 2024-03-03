29690fa4fcb6f710133897896832a5d9da3dc701
Andrej SH <Andrej.sh2017@mail.ru>
2024-03-03 14:35:36 +0300

    Добавьте файлы проекта.

---
+++ Kontr_9_1-Cpp-ITM0/Kontr_9_1-Cpp-ITM0.cpp
@@ -0,0 +1,82 @@
+﻿// Kontr_9_1-Cpp-ITMO.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <cmath>
+#include <iostream>
+#include <stdexcept>
+# include <windows.h>
+
+using namespace std;
+
+// Класс треугольник
+class Triangle {
+public:
+    Triangle(double a, double b, double c) : said_1(a), said_2(b), said_3(c) {
+
+    }
+
+    // Нахождение площади треугольника
+    double Square() const {
+
+        double p = (said_1 + said_2 + said_3) / 2;
+        double S = sqrt(p * (p - said_1) * (p - said_2) * (p - said_3));
+        return S;
+    }
+
+private:
+    double said_1;
+    double said_2;
+    double said_3;
+};
+
+
+void CheckingTrinagle(double a1, double b1, double a2, double b2, double a3, double b3) {
+    double said_a = (sqrt(pow((abs(a2 - a1) + abs(b2 - b1)), 2)));
+    double said_b = (sqrt(pow((abs(a3 - a2) + abs(b3 - b2)), 2)));
+    double said_c = (sqrt(pow((abs(a1 - a3) + abs(b1 - b3)), 2)));
+
+    if ((said_a > (said_b + said_c)) || (said_b > (said_a + said_c)) || (said_c > (said_b + said_a))) {
+        throw ((said_a + 100 > (said_b + said_c)) || (said_b + 100 > (said_a + said_c)) || (said_c + 100 > (said_b + said_a)));
+    }
+    else {
+        Triangle triangle{ said_a, said_b, said_c };
+        wcout << L"Площадь треугольника = " << triangle.Square() << endl;
+    }
+
+
+};
+
+
+int main() {
+
+    setlocale(LC_CTYPE, "rus");
+    double a1, b1, a2, b2, a3, b3;
+
+    cout << "Введите координату точки треугольника 'a1' ";
+    cin >> a1;
+    cout << "Введите координату точки треугольника треугольника 'b1' ";
+    cin >> b1;
+    cout << "Введите координату точки треугольника треугольника 'a2' ";
+    cin >> a2;
+    cout << "Введите координату точки треугольника треугольника 'b2' ";
+    cin >> b2;
+    cout << "Введите координату точки треугольника треугольника 'a3' ";
+    cin >> a3;
+    cout << "Введите координату точки треугольника треугольника 'b3' ";
+    cin >> b3;
+    CheckingTrinagle(a1, b1, a2, b2, a3, b3);
+
+}
+
+
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ Kontr_9_2-Cpp-ITMO/Kontr_9_2-Cpp-ITMO.cpp
@@ -0,0 +1,253 @@
+﻿// Kontr_9_2-Cpp-ITMO.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+# include <windows.h>
+#include <iomanip>
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Время в формате часы, минуты, секунды
+class Time {
+public:
+
+    int hours;
+    int minutes;
+    int seconds;
+
+    Time() : Time(0, 0, 0, false) {}
+
+    Time(int hours, int minutes, int seconds) : Time(hours, minutes, seconds, true) {
+        CheckingTime(hours, minutes, seconds);
+    }
+
+    // Для прибовления или вычитания времени к или от другому времени
+    int To_Time_Other() {
+        int a2b2c2 = 0;
+        a2b2c2 = CheckingTime(hours, minutes, seconds);
+        int hours1 = a2b2c2 / 10000;
+        int minutes1 = (a2b2c2 % 10000) / 100;
+        int seconds1 = a2b2c2 % 100;
+
+        return hours1 * 10000 + minutes1 * 100 + seconds1 * 1;
+    }
+
+    Time Result(int resultTime) {
+        int hours11 = resultTime / 10000;
+        int minutes11 = (resultTime % 10000) / 100;
+        int seconds11 = resultTime % 100;
+        int a2b2c2 = 0;
+        a2b2c2 = CheckingTime(hours11, minutes11, seconds11);
+        int hours22 = a2b2c2 / 10000;
+        int minutes22 = (a2b2c2 % 10000) / 100;
+        int seconds22 = a2b2c2 % 100;
+        return Time(hours22, minutes22, seconds22);
+    }
+
+    // Прибавление времени
+    Time Add(Time& other) {
+        Result(other.To_Time_Other() + To_Time_Other());
+        return Result(other.To_Time_Other() + To_Time_Other());
+    }
+
+    // Вычитание времени
+    Time Sub(Time& other) {
+        Result(To_Time_Other() - other.To_Time_Other());
+        return Result(To_Time_Other() - other.To_Time_Other());
+    }
+
+    // Перерасчёт времени если больше напечатано
+    int  CheckingTime(int a, int b, int c) {
+        int a1 = a;
+        int b1 = b;
+        int c1 = c;
+        bool marker0 = false;
+        bool marker1 = false;
+        bool marker2 = false;
+        bool marker3 = false;
+        bool marker4 = false;
+        bool marker5 = false;
+        bool marker6 = false;
+        bool marker7 = false;
+    label:
+
+        if (c1 < 0) {
+            b1 = b1 - 1;
+            c1 = 60 - abs(c1);
+            marker7 = true;
+        }
+        else {
+            marker7 = false;
+        }
+
+        if (b1 < 0) {
+            a1 = a1 - 1;
+            b1 = 60 - abs(b1);
+            marker6 = true;
+        }
+        else {
+            marker6 = false;
+        }
+
+        if (a1 < 0) {
+            a1 = 24 - abs(a1);
+            marker5 = true;
+        }
+        else {
+            marker5 = false;
+        }
+
+        if (c1 >= 60) {
+            b1 = b1 + 1;
+            c1 = c1 - 60;
+            marker4 = true;
+        }
+        else {
+            marker4 = false;
+        }
+
+        if (b1 >= 60) {
+            a1 = a1 + 1;
+            b1 = b1 - 60;
+            marker3 = true;
+        }
+        else {
+            marker3 = false;
+        }
+        if (a1 >= 24) {
+            a1 = a1 - 24;
+            marker2 = true;
+        }
+        else {
+            marker2 = false;
+        }
+
+        if (a1 >= 24 || b1 >= 60 || c1 >= 60 || a1 < 0 || b1 < 0 || c1 < 0) {
+            goto label;
+        }
+
+        if (marker2 == true || marker3 == true || marker4 == true || marker5 == true || marker6 == true || marker7 == true) {
+            wcout << L"\n-не корректное отображение времени, перерасчитанное время = ";
+            printf("%02d", a1);
+            cout << ".";
+            printf("%02d", b1);
+            cout << ".";
+            printf("%02d", c1);
+            marker1 == false;
+            int a1b1c1 = a1 * 10000 + b1 * 100 + c1 * 1;
+
+            marker0 = false;
+            marker1 = false;
+            marker2 = false;
+            marker3 = false;
+            marker4 = false;
+            marker5 = false;
+            marker6 = false;
+            marker7 = false;
+
+            return a1b1c1;
+        }
+        else {
+            int a1b1c1 = a1 * 10000 + b1 * 100 + c1 * 1;
+            return a1b1c1;
+        }
+    };
+
+    // [out] time-Время, в которое будет считана информация
+    friend istream& operator>>(istream& in, Time& time) {
+        in >> time.hours;
+        in.get();
+        in >> time.minutes;
+        in.get();
+        in >> time.seconds;
+        int a3b3c3 = 0;
+        a3b3c3 = time.hours * 10000 + time.minutes * 100 + time.seconds * 1;
+        time.Result(a3b3c3);
+
+        return in;
+    }
+    // time-Время, которое нужно вывести в поток
+    friend ostream& operator<<(ostream& out, const Time& time) {
+        out << setw(2) << setfill('0') << time.hours;
+        out << ':';
+        out << setw(2) << setfill('0') << time.minutes;
+        out << ':';
+        out << setw(2) << setfill('0') << time.seconds;
+
+        return out;
+    }
+
+private:
+    Time(int hours, int minutes, int seconds, bool validate) : hours(hours), minutes(minutes), seconds(seconds) {
+    }
+
+};
+
+class Validate // вложенный класс
+{
+public:
+    int V_hours;
+    int V_minutes;
+    int V_seconds;
+
+    void Val(Time t) {
+        //int hours1 = Time::Var_a(V_hours);
+        int hours1 = t.hours;
+        int minutes1 = t.minutes;
+        int seconds1 = t.seconds;
+        if ((hours1 >= 24 || hours1 < 0)) {
+            cout << "\ninvalid_argument - Hours value must be from 0 to 24" << endl;
+            throw invalid_argument{ "0" };
+        }
+        if ((minutes1 >= 60 || hours1 < 0)) {
+            cout << "\ninvalid_argument - minutes value must be from 0 to 60" << endl;
+            throw invalid_argument{ "0" };
+        }
+        if ((seconds1 >= 60 || hours1 < 0)) {
+            cout << "\ninvalid_argument - seconds value must be from 0 to 60" << endl;
+            throw invalid_argument{ "0" };
+        }
+    }
+
+};
+
+int main() {
+
+    setlocale(LC_CTYPE, "rus");
+
+    wcout << L"Введите первое время \"HH:MM:SS\" через пробел или поочереди: " << endl;
+    Time t1_time;
+    Validate t1;
+    wcout << L" - \"HH:MM:SS\:";
+    cin >> t1_time;
+    t1.Val(t1_time);
+
+    wcout << L"\n\nВведите второе время \"HH:MM:SS\" через пробел или поочереди: " << endl;
+
+    Time t2_time;
+    wcout << L" - \"HH:MM:SS\:";
+    cin >> t2_time;
+    t1.Val(t2_time);
+
+    Time time_result = t1_time.Add(t2_time);
+
+    wcout << L"\n- первое время + второе время = ";
+    cout << time_result << endl;
+    cout << "--------------" << endl;
+
+    time_result = t1_time.Sub(t2_time);
+    wcout << L"\n- первое время - второе время = ";
+    cout << time_result << endl;
+    cout << "--------------" << endl;
+
+}
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
